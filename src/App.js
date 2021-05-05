21bad8285816132bedaec79d8189d234147ec980
Veronika Ozretic <vozret00@fesb.hr>
2021-05-05 15:43:23 +0200

    useMemo hook (160)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 // useCallback -> so that we can use React.memo on the Button component
 
 import "./App.css";
-import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
+//import DemoOutput from "./components/Demo/DemoOutput";
 import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState("My List");
 
-  console.log("APP RUNNING!");
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
 
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prevState) => !prevState);
-    }
-  }, [allowToggle]);
-
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  };
+  const listItems = useMemo(() => [5, 1, 3, 10, 9], []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow toggling!</Button>
-      <Button onClick={toggleParagraphHandler}>Show paragraph!</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change Title!</Button>
     </div>
   );
 }
